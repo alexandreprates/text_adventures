bda51f0e450d8c447e463b23729af11de2ae4edf
Alexandre Prates <ajfprates@gmail.com>
2014-06-20 08:54:03 -0300

    Adding docs

---
+++ doc/js/search_index.js
@@ -0,0 +1,1 @@
+var search_data = {"index":{"searchIndex":["readme"],"longSearchIndex":[""],"info":[["README","","README_rdoc.html","","<p>Text Adventures\n<p>Text Adventures is a simple text dungeon generator to play over web.\n<p>Using RPG like concepts …\n"]]}}
