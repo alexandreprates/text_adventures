faae64e58ce61ecbdf1dae22ec44c0f5e7c89922
Alexandre Prates <ajfprates@gmail.com>
2026-06-06 10:27:42 -0300

    Add binary end-to-end coverage

---
+++ spec/e2e/text_adventures_binary_spec.rb
@@ -0,0 +1,33 @@
+require 'open3'
+require 'spec_helper'
+
+RSpec.describe "text_adventures binary" do
+  let(:root) { File.expand_path("../..", __dir__) }
+  let(:binary) { File.join(root, "bin", "text_adventures") }
+
+  it "runs a playable terminal session through the binary" do
+    output, error, status = Open3.capture3(
+      binary,
+      stdin_data: <<~COMMANDS
+        level
+        skills
+        go blacksmith
+        buy iron dagger
+        agree
+        equip iron dagger
+        inventory
+        quit
+      COMMANDS
+    )
+
+    expect(status).to be_success
+    expect(error).to eq ""
+    expect(output).to include "Welcome to Text Adventures"
+    expect(output).to include "Adventurer level 1"
+    expect(output).to include "Dagger Mastery: level 1 (0/50 XP)"
+    expect(output).to include "You bought Iron Dagger."
+    expect(output).to include "Equipped Iron Dagger."
+    expect(output).to include "weapon: Iron Dagger (Atk: 12)"
+    expect(output).to include "Thanks for playing."
+  end
+end
