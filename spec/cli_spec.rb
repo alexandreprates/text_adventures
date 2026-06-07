4e5e9f5208d35ebcdea123d60c7ccf5a01e08b15
Alexandre Prates <ajfprates@gmail.com>
2026-06-06 22:34:10 -0300

    Add interactive command history

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -4,6 +4,20 @@ require 'stringio'
 load File.expand_path("../bin/text_adventures", __dir__)
 
 RSpec.describe TextAdventures::CLI do
+  class FakeLineReader
+    attr_reader :prompts
+
+    def initialize(commands)
+      @commands = commands
+      @prompts = []
+    end
+
+    def call(prompt)
+      prompts << prompt
+      @commands.shift
+    end
+  end
+
   it "runs a terminal game loop until the player quits" do
     input = StringIO.new("inventory\nquit\n")
     output = StringIO.new
@@ -25,5 +39,19 @@ RSpec.describe TextAdventures::CLI do
 
     expect(output.string).to include "Town > You go to Ruins."
     expect(output.string).to include "Ruins L1 > "
+  end
+
+  it "uses an interactive line reader with scene prompts when available" do
+    output = StringIO.new
+    line_reader = FakeLineReader.new(["go ruins", "quit"])
+
+    described_class.new(output: output, line_reader: line_reader).run
+
+    expect(line_reader.prompts).to eq [
+      "\nTown > ",
+      "\nRuins L1 > "
+    ]
+    expect(output.string).to include "You go to Ruins."
+    expect(output.string).to include "Thanks for playing."
   end
 end
